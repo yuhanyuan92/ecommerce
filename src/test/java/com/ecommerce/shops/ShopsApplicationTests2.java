fda11ad1bff0473ce91df256629a43ef40cb098b
liushuangling <lsl_5039@163.com>
2019-01-17 14:22:18 +0800

    git测试新加类啊  狗子啊

---
+++ src/test/java/com/ecommerce/shops/ShopsApplicationTests2.java
@@ -0,0 +1,20 @@
+package com.ecommerce.shops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopsApplicationTests2 {
+    //git test  哈哈
+    //狗子啊  你好你好
+    //狗子啊狗子 啊  啊 啊啊啊
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
