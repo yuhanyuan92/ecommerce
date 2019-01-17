2cb1124d6be4c5660ea4c6ec23a0a655e2b2cff8
liushuangling <lsl_5039@163.com>
2019-01-17 14:38:14 +0800

    git测试新增类啊  狗子啊3

---
+++ src/test/java/com/ecommerce/shops/ShopsApplicationTests3.java
@@ -0,0 +1,20 @@
+package com.ecommerce.shops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopsApplicationTests3 {
+
+    //狗子啊狗子 啊  啊 啊啊  再试一次
+    //新增类
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
